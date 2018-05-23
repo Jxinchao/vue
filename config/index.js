e673f7154f13c8a41821a3fa9ad4ec7ea8ebefa2
jxinchao <425775961@qq.com>
2018-05-23 18:02:37 +0800

    login

--- config/index.js
+++ config/index.js
@@ -7,10 +7,24 @@ const path = require('path')
 module.exports = {
   dev: {
 
-    // Paths
+    // 静态资源文件夹
     assetsSubDirectory: 'static',
+    // 发布路径
     assetsPublicPath: '/',
-    proxyTable: {},
+    //代理配置表，在这里可以配置特定得请求代理到对应得API接口
+    // 列如将localhost:8080/api/xxx 代理到 node.itcnyz.com
+    proxyTable: { // 反向代理
+      '/api': {
+        targer: 'http://node.itcnyz.com', // 访问地址
+        // secure: false,   //如果https接口，需要配置这个参数
+        changeOrigin: true,  // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {   // 原本的接口是没有api前缀的，所以需要通过 pathRewrite 来重写地址
+          '^api':''
+        }
+        //接口地址原本是 /save/index，但是为了匹配代理地址，在前面加一个 /api,  因此接口地址需要写成这样的即可生效 /api/save/index。
+        // 注意： '/api' 为匹配项，target 为被请求的地址，因为在 ajax 的 url 中加了前缀 '/api'，而原本的接口是没有这个前缀的，所以需要通过 pathRewrite 来重写地址，将前缀 '/api' 转为 '/'。如果本身的接口地址就有 '/api' 这种通用前缀，就可以把 pathRewrite 删掉。
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -20,7 +34,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
---
+++ src/components/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login">
+      用户名：<input type="text">
+      <br><br>
+      密码：  <input type="password">
+      <br><br>
+      <input type="button" value="登陆">
+  </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: '',
+        data () {
+            return {
+                msg: ''
+            }
+        },
+        methods:{
+          axios.post('/login',{
+            uname:'',
+            upwd:''
+          },then((res)=>{
+            console.log(1)
+          },catch((res)=>{
+            console.log(1)
+          }))
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+$background: #fff;
+.login{
+  background:$background
+}
+</style>
