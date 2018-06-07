2d5ad4f4b12e3f20c555910a74a0d1efa3a22b7d
jxinchao <425775961@qq.com>
2018-06-07 18:01:16 +0800

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,9 @@
     {{ $route.params.id }}
       <router-link to="/login">登录</router-link>
       <router-link to="/register">注册</router-link>
+      <p>这里是vueX状态的值 {{ count }}</p>
+      <button @click="adds">+1</button>
+      <button @click="removes">-1</button>
   </div>
 </template>
 
@@ -13,6 +16,19 @@ export default {
     return {
       msg: ''
     }
+  },
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
+  },
+  methods:{
+    adds(){
+      this.$store.commit('add')
+    },
+    removes(){
+      this.$store.commit('remove')
+    },
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,17 +3,35 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 // import axios from 'axios';
 
+Vue.use(Vuex);
 // Vue.prototype.$axios = axios;
 // axios.defaults.baseURL = 'http://node.itcnyz.com:8001';
 // window.axios = require('axios')
 // Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  // vuex的配置
+  state:{
+    count:0
+  },
+  mutations:{
+    add(state){
+      state.count ++;
+    },
+    remove(state){
+      state.count --;
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
